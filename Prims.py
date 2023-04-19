8ac2c6bcdc32efb48348640029679ac072c94119
Josh <joshuatylermartinez@gmail.com>
2023-04-19 11:44:28 -0400

    Made graph functional and displayable

--- Prims.py
+++ Prims.py
@@ -5,18 +5,14 @@ import matplotlib.pyplot as plt
 
 
 G = nx.read_weighted_edgelist("./PrimsGraphEdgeList.txt")
-pos = nx.nx_agraph.graphviz_layout(G)
+pos = nx.spring_layout(G, k=1, seed=10)
 mst = nx.minimum_spanning_edges(G, algorithm="prim", data=False)
 edgelist = list(mst)
 print(edgelist)
 
-'''
-f = open("demofile3.txt", "w")
-f.write(edgelist)
-f.close()
-mst1 = nx.read_edgelist("demofile3.txt")
-nx.draw(mst1,with_labels=True)
-nx.draw_circular(G)
+nx.draw(G,pos, with_labels=True, )
+edge_labels = nx.get_edge_attributes(G, "weight")
+nx.draw_networkx_edge_labels(G, pos, edge_labels)
 
 plt.savefig('./test.png')
-'''
+
