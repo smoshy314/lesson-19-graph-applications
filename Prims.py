7abda958b0cf2ded9ba6b1991d46fcf272485b20
Josh <joshuatylermartinez@gmail.com>
2023-04-19 13:57:19 -0400

    Final graph

--- Prims.py
+++ Prims.py
@@ -11,9 +11,9 @@ mst = nx.minimum_spanning_edges(G, algorithm="prim", data=False)
 edgelist = list(mst)
 print(edgelist)
 
-nx.draw(G,pos, with_labels=True,node_shape="d", font_size=10 )
+nx.draw(G,pos, with_labels=True,node_shape="o", font_size=10, node_size=500, node_color="#d4e1ff", linewidths=.5 )
 edge_labels = nx.get_edge_attributes(G, "weight")
-nx.draw_networkx_edge_labels(G, pos, edge_labels)
+nx.draw_networkx_edge_labels(G, pos, edge_labels,font_size=8 )
 
 plt.savefig('./AcedemicBuildingGraph.png')
 
